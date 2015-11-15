fdfa00d417bd957942f69c7ee61d89040cbde4b8
Kaplan ERCAN <texasdandi@gmail.com>
2015-11-15 13:47:04 +0200

    New Command Line is added
    Signed-off-by: Kaplan ERCAN <texasdandi@gmail.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,12 +7,6 @@
  *  % java HelloWorld
  *  Hello, World
  *
- *  These 17 lines of text are comments. They are not part of the program;
- *  they serve to remind us about its properties. The first two lines tell
- *  us what to type to compile and test the program. The next line describes
- *  the purpose of the program. The next few lines give a sample execution
- *  of the program and the resulting output. We will always include such 
- *  lines in our programs and encourage you to do the same.
  *
  ******************************************************************************/
 
@@ -20,6 +14,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
+        System.out.println("Welcome, Commander");
         System.out.println("Hello, World");
     }
 
